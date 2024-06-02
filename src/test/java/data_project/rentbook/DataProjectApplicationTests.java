61c0d5a150e3e024abb1e9b0cb6d7fa390ab4aa0
jungjoo <wjddis000507@gmail.com>
2024-06-02 14:51:17 +0900

    테스트 에러 수정

--- src/test/java/data_project/rentbook/DataProjectApplicationTests.java
+++ src/test/java/data_project/rentbook/DataProjectApplicationTests.java
@@ -3,7 +3,6 @@ package data_project.rentbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DataProjectApplicationTests {
 
 	@Test
